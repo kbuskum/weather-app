49e853be302b94e082f6bee667cc0c920ac098fa
Kristine <gbuskum@gmail.com>
2022-12-01 11:30:24 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -98,7 +98,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "6b9a6eca05ee2de50dd49bd95f04b0af";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
